714fe50067eea04d4b22d210cf42abf91053633f
venomcrane <brandon_gachuz20@hotmail.com>
2025-06-30 23:47:17 -0600

    colors

--- netrecon.sh
+++ netrecon.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-#Colours
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+
+# ========== Colores (negrita + nombres descriptivos) ==========
+# --- Colores normales ---
+blackColour='\033[1;38;5;0m'      # #0a0f0a (negro oscuro)
+redColour='\033[1;38;5;1m'        # #ff0059 (rojo intenso)
+greenColour='\033[1;38;5;2m'       # #00ff41 (verde neón)
+yellowColour='\033[1;38;5;3m'      # #b3ff00 (amarillo verdoso)
+blueColour='\033[1;38;5;4m'        # #00b3ff (azul eléctrico)
+purpleColour='\033[1;38;5;5m'      # #bb00ff (morado vibrante)
+turquoiseColour='\033[1;38;5;6m'        # #00ffd2 (cian brillante)
+grayColour='\033[1;38;5;7m'       # #d9e0ee (blanco suave)
+
+# --- Colores brillantes ---
+strongGray='\033[1;38;5;8m'        # #262a2e (gris oscuro)
+lightRedColour='\033[1;38;5;9m'    # #ff477f (rosa fluorescente)
+limaColour='\033[1;38;5;10m'  # #77ff00 (verde limón)
+lightYellowColour='\033[1;38;5;11m' # #ccff00 (amarillo puro)
+lightBlueColour='\033[1;38;5;12m'   # #33ccff (azul cielo)
+lightPurpleColour='\033[1;38;5;13m' # #ff77ff (rosa chicle)
+lightCyanColour='\033[1;38;5;14m'   # #00ffff (cian encendido)
+brightWhiteColour='\033[1;38;5;15m' # #ffffff (blanco puro)
+
+# --- Reset ---
+endColour='\033[0m'
+
+
 
 #Ctrl+C 
 function ctrl_c(){
@@ -19,15 +36,14 @@ function ctrl_c(){
 trap ctrl_c INT
 
 
-banner="${greenColour}$(cat << "EOF"                                                                                                                                 
-
+banner="${turquoiseColour}$(cat << "EOF"                                                                                                                                 
 
          _______          __                       __     __________                            
          \      \   _____/  |___  _  _____________|  | __ \______   \ ____   ____  ____   ____  
          /   |   \_/ __ \   __\ \/ \/ /  _ \_  __ \  |/ /  |       _// __ \_/ ___\/  _ \ /    \ 
         /    |    \  ___/|  |  \     (  <_> )  | \/    <   |    |   \  ___/\  \__(  <_> )   |  \
         \____|__  /\___  >__|   \/\_/ \____/|__|  |__|_ \  |____|_  /\___  >\___  >____/|___|  /
-                \/     \/                              \/         \/     \/     \/           \/  [v 1.0] 
+                \/     \/                              \/         \/     \/     \/           \/   
                                              -=[ by r4venn ]=-                                        
 
 
@@ -158,12 +174,15 @@ udp_scan(){
 # Función de ayuda
 function helpPanel(){
   echo -e "$banner"
-  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Usage:${endColour} \n\t${purpleColour}./netrecon.sh [options] [arguments]${endColour}"
-  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Options:${endColour}"
-  echo -e "  ${blueColour}-t${endColour} ${purpleColour}<IP>${endColour}     ➜ ${grayColour}Perform Full TCP Scan${endColour}"
-  echo -e "  ${blueColour}-u${endColour} ${purpleColour}<IP>${endColour}     ➜ ${grayColour}Perform UDP Scan${endColour}"
-  echo -e "  ${blueColour}-h${endColour}          ➜ ${grayColour}Show Help Panel${endColour}"
-  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Examples:${endColour}"
+  echo -e "${limaColour}\n========================================================================================================="
+  echo -e "                                                Help Panel "
+  echo -e "=========================================================================================================${endColour}"
+  echo -e "\n${limaColour}[+]${endColour} ${grayColour}Usage:${endColour} \n\t${turquoiseColour}./netrecon.sh [options] [arguments]${endColour}"
+  echo -e "\n${limaColour}[+]${endColour} ${grayColour}Options:${endColour}"
+  echo -e "  ${turquoiseColour}-t${endColour} ${purpleColour}<IP>${endColour}     ➜ ${grayColour}Perform Full TCP Scan${endColour}"
+  echo -e "  ${turquoiseColour}-u${endColour} ${purpleColour}<IP>${endColour}     ➜ ${grayColour}Perform UDP Scan${endColour}"
+  echo -e "  ${turquoiseColour}-h${endColour}          ➜ ${grayColour}Show Help Panel${endColour}"
+  echo -e "\n${limaColour}[+]${endColour} ${grayColour}Examples:${endColour}"
   echo -e "\t${greenColour}./netrecon.sh -t 10.10.10.248${endColour}"
   echo -e "\t${greenColour}./netrecon.sh -u 10.10.10.248${endColour}"
   
